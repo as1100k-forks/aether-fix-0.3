623c2bcab1e095d0bc969b8b12d82a8ea0912cf3
Aditya Kumar <117935160+AS1100K@users.noreply.github.com>
2024-08-27 10:34:01 +0530

    fix command parsing

--- aether-core/src/command.rs
+++ aether-core/src/command.rs
@@ -7,19 +7,43 @@ pub enum AetherCommand {
 }
 
 impl AetherCommand {
-    pub fn parse(input: &str, state: &Bot) -> Self {
-        let parts: Vec<&str> = input.split_whitespace().collect();
+    // pub fn parse(input: &str, state: &Bot) -> Self {
+    //     let parts: Vec<&str> = input.split_whitespace().collect();
+    //
+    //     // TODO: Port all the commands to discord and only support necessary ones via minecraft chat
+    //     match parts.as_slice() {
+    //         ["!pearl", "load"] => {
+    //             if state.role == Role::Pearl {
+    //                 AetherCommand::PearlLoad
+    //             } else {
+    //                 AetherCommand::Unknown
+    //             }
+    //         }
+    //         ["!help"] => AetherCommand::Help,
+    //         _ => AetherCommand::Unknown,
+    //     }
+    // }
 
-        // TODO: Port all the commands to discord and only support necessary ones via minecraft chat
-        match parts.as_slice() {
-            ["!pearl", "load"] => {
-                if state.role == Role::Pearl {
-                    AetherCommand::PearlLoad
-                } else {
-                    AetherCommand::Unknown
+    // This is a temporary solution as azalea gives message twice
+    // and this way we can ignore the words after
+    pub fn parse(input: &str, state: &Bot) -> Self {
+        let mut parts = input.split_whitespace();
+
+        // Take the first word as the command
+        match parts.next() {
+            Some("!pearl") => {
+                match parts.next() {
+                    Some("load") => {
+                        if state.role == Role::Pearl {
+                            AetherCommand::PearlLoad
+                        } else {
+                            AetherCommand::Unknown
+                        }
+                    }
+                    _ => AetherCommand::Unknown,
                 }
             }
-            ["!help"] => AetherCommand::Help,
+            Some("!help") => AetherCommand::Help,
             _ => AetherCommand::Unknown,
         }
     }
